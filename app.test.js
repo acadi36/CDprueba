f4a9c668c4350ac812410fd55cd1c32297fa3dd1
Cayas Diaz Abraham <acadi2107@gmail.com>
2025-10-02 00:36:34 -0500

    Actualizacion 1 de app.test.js

--- app.test.js
+++ app.test.js
@@ -9,6 +9,6 @@ describe('GET /', () => {
   it('debería responder con el texto correcto', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toContain('¡PRUEBA 1 DE DESPLIEGUE CONTINUO CD!');
+    expect(response.text).toContain('¡PRUEBA 2 DE DESPLIEGUE CONTINUO CD!');
   });
 });
